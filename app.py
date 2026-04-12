e332d2c53f397b47b0832f96570c078d0401978e
Will Phoenix <will.phoenix@slu.edu>
2026-04-12 17:56:53 -0500

    Disable Gradio SSR on Space startup

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from mithridatium.gradio_app import UI_CSS, build_app
 demo = build_app()
 
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860, css=UI_CSS)
+    demo.launch(server_name="0.0.0.0", server_port=7860, css=UI_CSS, ssr_mode=False)
